8af83d285602cdaff0d4140b51697a8cfb489d43
kathyleboeuf <kleboeuf@townisp.com>
2018-10-15 12:02:42 -0400

    landing page done

--- src/components/landing-page.js
+++ src/components/landing-page.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import {connect} from 'react-redux';
-import {Link, Redirect} from 'react-router-dom';
+import { connect } from 'react-redux';
+import { Link, Redirect } from 'react-router-dom';
 
-import LoginForm from './login-form';
+// import LoginForm from './login-form';
 
 export function LandingPage(props) {
     // If we are logged in redirect straight to the user's dashboard
@@ -12,8 +12,10 @@ export function LandingPage(props) {
 
     return (
         <div className="home">
-            <h2>Welcome to Foo App</h2>
-            <LoginForm />
+            <h2>Idadakimasu!</h2>
+            <p>Do you know what to order in a Japanese restaurant?  Itadakimasu! will help you learn the names of different dishes. Try it out.</p>
+            {/* <LoginForm /> */}
+            <Link to="/login-form">Log In</Link>
             <Link to="/register">Register</Link>
         </div>
     );

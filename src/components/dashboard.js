3ed5453ae8178c66425a95339c2c88a7110c4c4d
kathyleboeuf <kleboeuf@townisp.com>
2018-10-16 12:32:48 -0400

    working getting question

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,61 +1,67 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import requiresLogin from './requires-login';
-//import { fetchProtectedData } from '../actions/protected-data';
 
 
 export class Dashboard extends React.Component {
-    componentDidMount() {
-        // this.props.dispatch(fetchProtectedData());
+  componentDidMount() {
+    this.props.dispatch(fetchQuestion());
+  }
+  render() {
+    // let number = this.props.number;
+    // let answers = this.props.answers;
+    if (!this.props.currentQuestion) {
+      return (
+        <div>
+          <img src="/img/gude_loading.png" />
+        </div>
+      );
     }
+    return (
+      <section className="dashboard">
+        <div className="dashboard-username">
+          Hello, {this.props.username}
+        </div>
 
-    render() {
-        let number = this.props.number;
-        let answers = this.props.answers;
-        return (
-            <div className="dashboard">
-                <div className="dashboard-username">
-                    Hello, {this.props.username}
-                </div>
+        <section classsName="dash-input">
+          <div className="dash-pic">
+            <img className="image" src={this.props.currentQuestion.imageURL} alt="this picture" />
+            <p>{this.props.currentQuestion.question}</p>
+          </div>
 
-          <section classsName="dash-input">
-                    <div className="dash-pic">
-                        <img className="image" src="/img/trebleBassHeart.png" alt="alternate food description" />
+          <div>
+            <input id="input-Answer "
+              className="input-Answer"
+              type="text" />
+          </div>
 
-                    </div>
-                    <div>
-                        <input id="input-Answer "
-                            className="input-Answer"
-                            type="text" />
-                    </div>
-                    <div>
-                        <button className="submit">Submit</button>
-                    </div>
-                    <div>
-                        <p>Right! or Sorry, the correct answer is ...</p>
-                    </div>
-                    <div>
-                        <p># {number} correct out of  all {answers}</p>
-                    </div>
-                    <div>
-                        <button className="next">Next</button>
-                    </div>
-                </section >
-            </div >
+          <div>
+            <button className="submit">Submit</button>
+          </div>
 
-               
-           
+          <div>
+            <p>Right! or Sorry, the correct answer is ...</p>
+          </div>
 
-        );
-    }
+          <div>
+            <p># {number} correct out of all {answers}</p>
+          </div>
+
+          <div>
+            <button className="next">Next</button>
+          </div>
+        </section >
+      </section >
+    );
+  }
 }
 const mapStateToProps = state => {
-    const { currentUser } = state.auth;
-    return {
-        username: state.auth.currentUser.username,
-        name: `${currentUser.firstName} ${currentUser.lastName}`,
-        protectedData: state.protectedData.data
-    };
+  const { currentUser } = state.auth;
+  return {
+    username: state.auth.currentUser.username,
+    name: `${currentUser.firstName} ${currentUser.lastName}`,
+    currentQuestion: state.question.currentQuestion
+  };
 };
 
 export default requiresLogin()(connect(mapStateToProps)(Dashboard));

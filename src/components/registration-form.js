88d6171738b716323c16cfe608af9ba409d5ae86
kathyleboeuf <kleboeuf@townisp.com>
2018-10-17 17:20:56 -0400

    fixing centering reg form

--- src/components/registration-form.js
+++ src/components/registration-form.js
@@ -53,12 +53,14 @@ export class RegistrationForm extends React.Component {
                     name="passwordConfirm"
                     validate={[required, nonEmpty, matchesPassword]}
                 />
-                <button
-                    className="btn-sub"
-                    type="submit"
-                    disabled={this.props.pristine || this.props.submitting}>
-                    Register
+                <div className="btn-div">
+                    <button
+                        className="btn-sub"
+                        type="submit"
+                        disabled={this.props.pristine || this.props.submitting}>
+                        Register
                 </button>
+                </div>
             </form>
         );
     }

ac22f92af7d337ba00674dd70ea296f30deaf1a0
kathyleboeuf <kleboeuf@townisp.com>
2018-10-18 10:27:06 -0400

    first profile commit

---
+++ src/components/profile-page.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+
+
+export class Profile extends React.Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			user: this.user,
+			questionsAsked: 0,
+			correct: 0,
+			allQuestionsAsked: 0,
+			allCorrect: 0,
+			question1Attempts: 0,
+			question1Correct: 0,
+			question2Attempts: 0,
+			question2Correct: 0,
+			question3Attempts: 0,
+			question3Correct: 0,
+			question4Attempts: 0,
+			question4Correct: 0,
+			question5Attempts: 0,
+			question5Correct: 0,
+			question6Attempts: 0,
+			question6Correct: 0,
+			question7Attempts: 0,
+			question7Correct: 0,
+			question8Attempts: 0,
+			question8Correct: 0,
+			question9Attempts: 0,
+			question9Correct: 0,
+			question10Attempts: 0,
+			question10Correct: 0,
+		}
+
+	}
+
+	render() {
+
+		return (
+
+
+
+	)
+	}
+
+}

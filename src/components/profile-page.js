97b5fe4d4200506673a3db5d44faae812da79898
kathyleboeuf <kleboeuf@townisp.com>
2018-10-18 12:30:52 -0400

    added if/else to profile loading page

--- src/components/profile-page.js
+++ src/components/profile-page.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Link, Redirect } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 import { fetchProgress } from '../actions/question';
 import './profile-page.css'
@@ -12,44 +12,50 @@ export class Profile extends React.Component {
 		this.props.dispatch(fetchProgress())
 	}
 
-	render() { //answer, attempts, successes, index
-		const userData = this.props.userData.map(question => (
-			<li key={question.index}>
-				<h3>Question {question.index}</h3>
-				<p>Answer is {question.answer} </p>
-				<p>{question.successes}&nbsp;&nbsp;out of&nbsp;&nbsp;{question.attempts}</p>
-			</li>
-		));
+	render() {
 
-
-		return (
-
-			<section className="profile">
+		if (!this.props.userData) {
+			return (
 				<div>
-					<h2>Your Profile</h2>
-					<h3>UserName{this.props.username}</h3>
-					<h3>Today's Progress</h3>
-					<p className="results">{this.state.correct}&nbsp;&nbsp;correct out of&nbsp;&nbsp;{this.state.questionsAsked}</p>
-
-					<h3>Overall Progress</h3>
-					<p className="results">{this.state.AllCorrect}&nbsp;&nbsp;correct out of&nbsp;&nbsp;{this.state.AllQuestionsAsked}</p>
+					<img src="/img/gude_loading.png" alt="gudetama resting" />
 				</div>
-				<div className="question-chart">
-					<h3>Question Progress</h3>
-					<ul>
-						{userData}
-					</ul>
-				</div>
+			);
+		} else {
+			const userData = this.props.userData.map(question => (
+				<li key={question.index}>
+					<h3>Question {question.index}</h3>
+					<p>Answer is {question.answer} </p>
+					<p>{question.successes}&nbsp;&nbsp;out of&nbsp;&nbsp;{question.attempts}</p>
+				</li>
+			));
 
-				<button className="btn-profile" >
-					<Link to="/dashboard" className="btn-profile">back to the game</Link>
-				</button>
+			return (
+				<section className="profile">
+					<div>
+						<h2>Your Profile</h2>
+						<h3>UserName{this.props.username}</h3>
+						<h3>Today's Progress</h3>
+						<p className="results">{this.state.correct}&nbsp;&nbsp;correct out of&nbsp;&nbsp;{this.state.questionsAsked}</p>
 
-			</section>
+						<h3>Overall Progress</h3>
+						<p className="results">{this.state.AllCorrect}&nbsp;&nbsp;correct out of&nbsp;&nbsp;{this.state.AllQuestionsAsked}</p>
+					</div>
+					<div className="question-chart">
+						<h3>Question Progress</h3>
+						<ul>
+							{userData}
+						</ul>
+					</div>
 
-		)
+					<button className="btn-profile" >
+						<Link to="/dashboard" className="btn-profile">back to the game</Link>
+					</button>
+
+				</section>
+
+			)
+		}
 	}
-
 }
 
 const mapStateToProps = state => ({
